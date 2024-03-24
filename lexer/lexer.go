2d2d0ca1181722ade8cc38b020c9c00f146e4586
Nathan Helmig <nathan.helmig@itility.com>
2024-03-24 10:27:28 -0500

    starting to tokenize keywords. TODO tokenize IDEN

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,6 +1,7 @@
 package lexer
 
 import (
+	"fmt"
 	"foo_lang/token"
 	"unicode/utf8"
 )
@@ -11,6 +12,7 @@ type Lexar struct {
 	readPosition int
 	ch           rune
 }
+
 
 func New(input string) *Lexar {
 	l := &Lexar{input: input}
@@ -54,9 +56,29 @@ func (l *Lexar) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 	l.readRune()
 	return tok
+}
+
+func (l *Lexar) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readRune()
+	}
+	return l.input[position:l.position]
+}
+func isLetter(ch rune) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
 }
 func newToken(tokenType token.TokenType, ch rune) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
