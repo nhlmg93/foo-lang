af8fedbdda9869a2e80fcd17918403f135fb1102
Nathan Helmig <nathan.helmig@itility.com>
2024-03-24 09:47:26 -0500

    can tokenize char/runes

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,6 +1,7 @@
 package lexer
 
 import (
+	"foo_lang/token"
 	"unicode/utf8"
 )
 
@@ -12,7 +13,11 @@ type Lexar struct {
 }
 
 func New(input string) *Lexar {
-	return &Lexar{input: input}
+	l := &Lexar{input: input}
+
+	l.readRune()
+
+	return l
 }
 
 func (l *Lexar) readRune() {
@@ -25,4 +30,34 @@ func (l *Lexar) readRune() {
 	}
 	l.position = l.readPosition
 	l.readPosition += width
+}
+
+func (l *Lexar) NextToken() token.Token {
+	var tok token.Token
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+	l.readRune()
+	return tok
+}
+func newToken(tokenType token.TokenType, ch rune) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
 }
