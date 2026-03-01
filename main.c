73d706b258ed6a7c0bf2445c07dcc67f6f065a82
Nathan Helmig <nhelmig93@gmail.com>
2026-03-01 16:13:05 -0600

    snake_case

--- main.c
+++ main.c
@@ -1,25 +1,52 @@
 #include <stdbool.h>
+#include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
 
+typedef struct Arena {
+  uint8_t *base;
+  size_t cap;
+  size_t off;
+} Arena;
+
+static inline uintptr_t align_forward_uintptr(uintptr_t p, size_t align) {
+  return (p + (align - 1)) & ~(uintptr_t)(align - 1);
+}
+
+void arena_init(Arena *a, void *backing, size_t cap) {
+  a->base = (uint8_t *)backing;
+  a->cap = cap;
+  a->off = 0;
+}
+
+void *arena_alloc_align(Arena *a, size_t size, size_t align) {
+  uintptr_t cur = (uintptr_t)a->base + (uintptr_t)a->off;
+  uintptr_t aligned = align_forward_uintptr(cur, align);
+  size_t newOff = (size_t)(aligned - (uintptr_t)a->base) + size;
+  if (newOff > a->cap)
+    return NULL;
+  a->off = newOff;
+  return (void *)aligned;
+}
+
 typedef char *TokenType;
 
-struct Token {
+typedef struct Token {
   TokenType type;
   char literal;
 } Token;
 
-struct Token newToken(TokenType type, char literal) {
+struct Token token_init(TokenType type, char literal) {
   struct Token t;
   t.type = type;
   t.literal = literal;
   return t;
 }
 
-void printToken(struct Token t) {
+void print_token(Token t) {
   printf("Token {.type= \"%s\", .literal= '%c'}\n", t.type, t.literal);
 }
 
@@ -44,27 +71,27 @@ void printToken(struct Token t) {
 
 typedef uint8_t byte;
 
-struct Lexer {
+typedef struct Lexer {
   char *input;
   int position;     // current pos in input
   int readPosition; // current reading pos
   byte ch;          // current char
 } Lexer;
 
-void printLexer(struct Lexer l) {
+void print_lexer(Lexer l) {
   printf(
       "Lexer {.input= \"%s\", .position= %d, .readPosition= %d, .ch= '%c'}\n",
       l.input, l.position, l.readPosition, l.ch);
 }
 
-struct Lexer newLexer(char *input) {
+struct Lexer lexer_init(char *input) {
   struct Lexer l;
-  memset(&l, 0, sizeof(struct Lexer));
+  memset(&l, 0, sizeof(Lexer));
   l.input = input;
   return l;
 }
 
-void readChar(struct Lexer *l) {
+void read_char(Lexer *l) {
   if (l->readPosition >= (int)(strlen(l->input))) {
     l->ch = 0;
   } else {
@@ -74,41 +101,41 @@ void readChar(struct Lexer *l) {
   l->readPosition++;
 }
 
-struct Token nextToken(struct Lexer *l) {
-  struct Token t;
+Token next_token(Lexer *l) {
+  Token t;
   switch (l->ch) {
   case '=':
-    t = newToken(ASSIGN, l->ch);
+    t = token_init(ASSIGN, l->ch);
     break;
   case '+':
-    t = newToken(PLUS, l->ch);
+    t = token_init(PLUS, l->ch);
     break;
   case ',':
-    t = newToken(COMMA, l->ch);
+    t = token_init(COMMA, l->ch);
     break;
   case ';':
-    t = newToken(SEMICOLON, l->ch);
+    t = token_init(SEMICOLON, l->ch);
     break;
   case '(':
-    t = newToken(LPAREN, l->ch);
+    t = token_init(LPAREN, l->ch);
     break;
   case ')':
-    t = newToken(RPAREN, l->ch);
+    t = token_init(RPAREN, l->ch);
     break;
   case '{':
-    t = newToken(LBRACE, l->ch);
+    t = token_init(LBRACE, l->ch);
     break;
   case '}':
-    t = newToken(RBRACE, l->ch);
+    t = token_init(RBRACE, l->ch);
     break;
   case 0:
     t.literal = '\0';
     t.type = EOF_TOKEN;
     break;
   default:
-    t = newToken(ILLEGAL, l->ch);
+    t = token_init(ILLEGAL, l->ch);
   }
-  readChar(l);
+  read_char(l);
   return t;
 }
 
@@ -119,21 +146,21 @@ int main(int argc, char *argv[]) {
   }
 
   char *input = argv[1];
-  struct Lexer l = newLexer(input);
+  Lexer l = lexer_init(input);
 
-  readChar(&l);
-  //printLexer(l);
+  read_char(&l);
+  // print_lexer(l);
 
-  struct Token t;
+  Token t;
   while (true) {
-    t = nextToken(&l);
-    //printLexer(l);
+    t = next_token(&l);
+    // print_lexer(l);
 
     if (strcmp(t.type, EOF_TOKEN) == 0) {
-      printToken(t);
+      print_token(t);
       break;
     } else {
-      printToken(t);
+      print_token(t);
     }
   }
 
